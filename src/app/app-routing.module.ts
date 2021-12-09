983ab2d4c63797686e5a342d6c331fecaef9aeff
llccss11 <quintana.lucas@gmail.com>
2021-12-09 18:18:44 -0300

    rutas ajustadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { CardsComponent } from './components/cards/cards.component';
 import { DetailsComponent } from './components/details/details.component';
 
 const routes: Routes = [
-  { path: 'home', redirectTo: '/', pathMatch: 'full' },
-  { path: '', component: CardsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: CardsComponent },
   { path: 'details', component: DetailsComponent },  
-  { path: '*', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
   
 ];
 
